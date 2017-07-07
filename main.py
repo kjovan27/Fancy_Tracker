403e900a00b332fb9d900dde864accafa159b229
lukyamuziB <lukyamuzibenon@gmail.com>
2017-07-07 15:55:57 +0300

    Update main.py
    resolved a type error

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class User(object):
 		print("Hello "+ self.user_name+" :)\nHere are the skills that are still incomplete: ")
 		for task in undone:
 			num = 1
-			print(num+". "+ task)
+			print(str(num)+". "+ task)
 			num += 1
 		return undone
 
