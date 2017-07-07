4d8e593db6ca3c71199f6f937aa1fa5ca74fcf44
kidepo <kiyingidenispaul@gmail.com>
2017-07-07 16:44:52 +0300

    Merged tests to the Master

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,14 @@
+from main import User
 from unittest import TestCase
-from src.main import User
 
 
 class TestUserClass(TestCase):
 
     def setUp(self):
-        self.new_user = User("Thegaijin")
+        try:
+            self.new_user = User("Thegaijin")
+        except NameError as e:
+            raise 'Check the class name and try again'
 
     def test_User_instance(self):
         self.assertIsInstance(
@@ -17,3 +20,10 @@ class TestUserClass(TestCase):
     def test_argument_data_types_for_User(self):
         self.assertIsInstance(
             self.new_user.user_name, str, msg="Argument should be a string")
+
+    def test_if_class_has_add_skills_method(self):
+        getattr(User, 'add_skills', 'None')
+
+    def test_argument_data_types_for_add_skills_function(self):
+        add_skills_call = self.new_user.add_skills(['Python', 'JavaScript'])
+        self.assertEqual(type(add_skills_call), list)
