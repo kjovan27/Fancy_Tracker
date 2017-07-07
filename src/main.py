655aed7dd4e0202ce1c89930a53bf9e249289fb8
S V <S V>
2017-07-07 11:53:23 +0300

    Adding main.py file with add_skills function and test_main.py file

---
+++ src/main.py
@@ -0,0 +1,12 @@
+class User(object):
+
+    def __init__(self, user_name):
+        self.user_name = user_name
+        self.skills = []
+        self.done_skills = []
+
+    def add_skills(self, skills_to_learn):
+        if skills_to_learn == None:
+            for skill in skills_to_learn:
+                if isinstance(skill, str):
+                    self.skills.append(skill)
