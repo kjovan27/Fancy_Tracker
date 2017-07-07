6b4d3c16d16a47c2386e11b40678c7c86aaca1a7
Jovan Kizito <kjovan27@gmail.com>
2017-07-07 05:25:47 -0700

    view_progress method

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 07 10:13:53 2017
 
-	def unstudied_skills(self):
-		""" this method enables the user view undone tasks"""
-		undone = [task for task in self.skills if task not in self.done_skills]
-		print("Hello "+ self.user_name+" :)\nHere are the skills that are still incomplete: ")
-		for task in undone:
-			num = 1
-			print(num+". "+ task)
-			num += 1
-		return undone
+@author: Don Quan
+"""
 
-
+def view_progress(self):
+    
+    progress_percentage = (float(len(self.done_skills))/float(len(self.skills))) * 100
+    
+    return str(progress_percentage + "%")
